707cc3dd786353933ba2d07a4dd7cfa7c68a9a23
srijanatb9 <srijanat@metropolia.fi>
2022-12-23 03:39:46 +0200

    Module 4. AJAX

--- Module 4/2.js
+++ Module 4/2.js
@@ -1,14 +1,30 @@
-'use strict';
+'use strict'
 
-async function showInfo() {
+const tvSeriesName = prompt('Enter the name of the TV series.')
 
-    const show = prompt('Enter the name of the show.')
+async function tvSeries() {
 
-    const response = await fetch(`https://api.tvmaze.com/singlesearch/shows?q=${show}`)
+    const response = await fetch(`https://api.tvmaze.com/singlesearch/shows?q=${tvSeriesName}`)
     const responseJSON = await response.json()
 
     console.log(responseJSON)
 
+    const name = responseJSON['name']
+    const image = responseJSON['image']['medium']
+    const genres = responseJSON['genres']
+    const summary = responseJSON['summary']
+    const linkToDetails = responseJSON['url']
+
+    document.getElementById('seriesName').innerText = name
+    document.getElementById('seriesImg').src = image
+    document.getElementById('genre').innerText = `Genre: ${genres[0]} | ${genres[1]} | ${genres[2]}`
+    document.getElementById('summary').innerHTML = summary
+    document.getElementById('url').href = linkToDetails
 }
 
-showInfo()
+if (tvSeriesName === "" || tvSeriesName === null) {
+    document.querySelector('#noName').innerHTML = 'No result'
+}
+else {
+    tvSeries()
+}
